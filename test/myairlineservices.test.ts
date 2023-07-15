9354bc835149025c06ff3eac31e97821310dd9fe
shankey28 <shashiishankar@gmail.com>
2023-07-15 14:38:47 -0400

    Initial commit

---
+++ test/myairlineservices.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Myairlineservices from '../lib/myairlineservices-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/myairlineservices-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Myairlineservices.MyairlineservicesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
